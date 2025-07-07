0651f82fb851d209e6b3f5fa2957cf3660f41801
Marcos André O dos Santos <m4rcos.oliveira19@gmail.com>
2025-07-07 20:36:32 -0300

    Update primiero_banco_SqLite.py

--- primiero_banco_SqLite.py
+++ primiero_banco_SqLite.py
@@ -1,6 +1,6 @@
 import sqlite3
 from sqlite3 import Error
- #############conexão
+ ##########conexão
 def conexao_banco():
   caminho = 'C:\\Users\\kkded\\OneDrive\\Área de Trabalho\\Projetos P\\Banco_com_SQLite\\contatos.db'
   con = None
@@ -11,24 +11,39 @@ def conexao_banco():
   return con    
 
 vcon = conexao_banco()
-#############criar tabela
-tabela1 = """CREATE TABLE CONTATOS(
+
+##########criar tabela
+criuar_tabela1 = """CREATE TABLE CONTATOS(
                 ID_CONTATO INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 NOME_CONTATO VARCHAR(30) NOT NULL,
                 TELEFONE_CONTATO VARCHAR (11) NOT NULL,
                 EMAIL_CONTATO VARCHAR(30) NOT NULL
 );"""
 
+##########inserir valores na tabela 
+inserir_valor = """INSERT INTO CONTATOS
+                      (NOME_CONTATO,TELEFONE_CONTATO,EMAIL_CONTATO)
+                  VALUES('Marcos André Oliveira dos Santos','(82)99876-5432-','m4rcos.oliveira19@gmail.com')"""
 def criar_tabela(conexao,sql):
   try:
-    c=conexao.cursor()
-    c.execute(sql)
+    cursor=conexao.cursor()
+    cursor.execute(sql)
     print('tabela criada')
   except Error as ex:
     print(ex)
 
+def inserir(conexao,sql):
+  try:
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+    cursor.commit()
+    print('Inserido com sucesso')
+  except Error as ex:
+    print(ex)
 
-criar_tabela(vcon,tabela1)
+##########CHAMADAS
+#criar_tabela(vcon,criuar_tabela1)
+inserir(vcon,inserir_valor)
 
 
 vcon.close()
