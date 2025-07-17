e961893a1c6d964d6931268be5522e2fc23e7273
Marcos André O dos Santos <m4rcos.oliveira19@gmail.com>
2025-07-16 23:32:44 -0300

    Update Agenda.py

--- Agenda.py
+++ Agenda.py
@@ -15,16 +15,16 @@ def conexao():
   return con
 
 vcon = conexao()
-
+#---------------------------------------------------------
 def menuprincipal():
   os.system("cls")
   print("1 - Inserir registro ")
   print("2 - Deletar registro ")
   print("3 - Atualizar registro ")
-  print("4 - Consultar registro por ID")
+  print("4 - Consultar registro ")
   print("5 - Consultar registro por Nome ")
   print("6 - Sair ")
-
+#---------------------------------------------------------
 def inserir(conexao):
   os.system('cls')
   nome = input('Nome completo: ')
@@ -42,7 +42,7 @@ def inserir(conexao):
     print('Dados inseridos com sucesso!')
   except Error as ex:
     print(ex)
-  
+#---------------------------------------------------------  
 def deletar(conexao):
   os.system('cls')
   n1 = int(input('Qual o ID dos dados que deseja deletar: '))
@@ -55,12 +55,51 @@ def deletar(conexao):
     print('dado deletado com sucesso')
   except Error as ex:
     print(ex)
-def atualizar():
+#---------------------------------------------------------
+def atualizar(conexao):
   os.system('cls')
-  
-def consultar_id():
-  print()
+  id_registro = input('Qual o ID que deseja atualizar?:')
 
+  print("\n--- Campos para Atualizar ---")
+  print("1- Nome completo")
+  print("2- Número contato")
+  print("3- CEP")
+  print("4- E-mail")
+  opcao_campo = int(input("Escolha o número de campo que deseja atualizar: "))
+
+  coluna = "" 
+  novo_valor = ""
+  sql = ""
+  
+  if opcao_campo == 1:
+    coluna = 'NOME_COMPLETO'
+    novo_valor = input('Qual é o novo nome?: ')
+  elif opcao_campo == 2:
+    coluna = 'NUMERO_CONTATO'
+    novo_valor = input('Qual é o novo CONTATO: ')
+  elif opcao_campo == 3:
+    coluna = 'CEP'
+    novo_valor = input('Qual é o novo CEP: ')
+  elif opcao_campo == 4:
+    coluna = 'EMAIL'
+    novo_valor = input('Qual é o novo E-mail: ')
+  else:
+    print('Infelizmente não temos essa opção, tente novamente!')
+    return
+  
+  sql = f"UPDATE AGENDA SET {coluna} = ? WHERE ID = ?"
+  dados = (novo_valor,id_registro)
+
+  try:
+    cursor = conexao.cursor()
+    cursor.execute(sql,dados)
+    conexao.commit()
+  except Error as ex:
+    print(ex)
+#---------------------------------------------------------
+def consultar():
+  print()
+#---------------------------------------------------------
 def consulta_nome():
   print()
 
@@ -77,7 +116,7 @@ while opcao != 6:
   elif opcao == 3:
     atualizar(vcon)
   elif opcao == 4:
-    consultar_id(vcon)
+    consultar(vcon)
   elif opcao == 5:
     consulta_nome(vcon)
   elif opcao == 6:
